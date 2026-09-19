c9cdfb5166db0d11b8c221c48200135c93853b9e
IEVN1001-23001069 <lunasolisort+escolar@gmail.com>
2026-09-19 10:03:40 -0600

    Pipes 19/09/2026 10:05 a.m.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,13 +1,18 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
+import { HeroesFilterPipe } from './heroes/heroes-filter-pipe';
 
 @NgModule({
-  declarations: [App, HeroesList],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [App, HeroesList, HeroesFilterPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
 export class AppModule {}
+
+//Para trabajar los formularios debemos de importar el modulo de formularios con la linea:
+// import { FormsModule } from '@angular/forms'; recuerda agregarlo al Imports dentro del @NgModule
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IHeroes } from '../heroes'; //Shift+Alt+. para hacer la importanción instantaneamente al agregar la interfaz
 
 @Component({
   selector: 'app-heroes-list',
@@ -6,4 +7,46 @@ import { Component } from '@angular/core';
   // styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+  heroes:IHeroes[]=[ //Construimos con un tipo de dato de angular que es cualquiera any y tipo de dato arreglo, ahora pasara a hacer referencia a una interfaz y sera de tipo IHeroes la interfaz que creamos
+    
+
+    {
+      imagen:'https://dragonball-api.com/characters/goku_normal.webp',
+      nombre:'Goku',
+      description:'¡¡¡¡¡¡Kame Hame Ha!!!!!!',
+      race:'Saiyan',
+      ki:60000000
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/bulma.webp',
+      nombre:'Bulma',
+      description:'Bulma es la protagonista femenina',
+      race:'Human',
+      ki:0
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/gohan.webp',
+      nombre:'Gohan',
+      description:'Es uno de los personajes principales',
+      race:'Saiyan',
+      ki:45000000
+    },
+    {
+      imagen:'https://dragonball-api.com/characters/ChiChi_DBS.webp',
+      nombre:'Chi Chi',
+      description:'Esposa de Goku y madre de Gohan.',
+      race:'Human',
+      ki:0
+    },
+  ]
+}
